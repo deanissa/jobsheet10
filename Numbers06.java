e132b7fe57b24d5c9afe6cdf9aa126abd0cff128
deanissa <deanissa604@gmail.com>
2023-11-02 21:09:13 +0700

    Menambahkan panjang baris (kolom)

--- Numbers06.java
+++ Numbers06.java
@@ -1,15 +1,22 @@
 /**
  * Numbers06
  */
-import java.util.Scanner;
+import java.util.Arrays;
 public class Numbers06 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
 
-        int[][] myNumbers= new int [3][];
+
+        int[][] myNumbers= new int [3][5];
         myNumbers[0] = new int [5];
         myNumbers[1] = new int [3];
         myNumbers[2] = new int [1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println (Arrays.toString(myNumbers[i]));
+        }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ":" + myNumbers[i].length);
+        }
     }
 }
